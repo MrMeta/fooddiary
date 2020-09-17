4015b25f45d197b51ba2570b9a51ef75c1e91e48
MrMeta <bungyo92@gmail.com>
2020-09-18 00:49:57 +0900

    Write basic test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from django.urls import resolve
+from app.views import food_list
 
-# Create your tests here.
+
+class FoodListTest(TestCase):
+
+    def test_root_url_resolves_to_food_list_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, food_list)
+
+    def test_food_list_returns_correct_html(self):
+        request = HttpRequest()
+        response = food_list(request)
+        self.assertTrue(response.content.startswith(b'\n<html>'))
+        self.assertTrue(response.content.endswith(b'</html>\n'))
