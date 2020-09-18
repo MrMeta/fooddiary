db2a7700c03d46530bc172eff299246385ed9f07
MrMeta <bungyo92@gmail.com>
2020-09-18 10:59:58 +0900

    Alter food.craeted_date field

--- app/models.py
+++ app/models.py
@@ -15,7 +15,7 @@ class Food(models.Model):
     store = models.ForeignKey(Store, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
-    created_date = models.DateField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
