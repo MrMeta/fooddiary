f594f2e8eb3bc19b0a6e03c49276b07bb33411da
MrMeta <bungyo92@gmail.com>
2020-09-18 12:00:54 +0900

    Add FoodReviewForm && test

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Food, Store
+from .models import Food, Store, FoodReview
 
 REQUIRED_ERROR_MESSAGE = "You must input this field"
 
@@ -7,14 +7,14 @@ REQUIRED_ERROR_MESSAGE = "You must input this field"
 class FoodForm(forms.models.ModelForm):
 
     class Meta:
-        model=Food
-        fields=('name', 'description', 'store')
+        model = Food
+        fields = ('name', 'description', 'store')
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'store': forms.Select(attrs={'class': 'form-control'}),
             'description': forms.TextInput(attrs={'class': 'form-control'}),
         }
-        error_messages={
+        error_messages = {
             'name': {'required': REQUIRED_ERROR_MESSAGE}
         }
 
@@ -22,12 +22,27 @@ class FoodForm(forms.models.ModelForm):
 class StoreForm(forms.models.ModelForm):
 
     class Meta:
-        model=Store
-        fields=('name', 'address')
+        model = Store
+        fields = ('name', 'address')
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'address': forms.TextInput(attrs={'class': 'form-control'}),
         }
-        error_messages={
+        error_messages = {
             'name': {'required': REQUIRED_ERROR_MESSAGE},
+        }
+
+
+class FoodReviewForm(forms.models.ModelForm):
+
+    class Meta:
+        model = FoodReview
+        fields = ('title', 'content')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        error_messages = {
+            'title': {'required': REQUIRED_ERROR_MESSAGE},
+            'content': {'required': REQUIRED_ERROR_MESSAGE},
         }
--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from app.forms import (
     FoodForm,
+    FoodReviewForm,
     REQUIRED_ERROR_MESSAGE,
 )
 
@@ -11,3 +12,16 @@ class FoodFormTest(TestCase):
         form = FoodForm(data={'name': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['name'], [REQUIRED_ERROR_MESSAGE])
+
+
+class FoodReviewFormTest(TestCase):
+
+    def test_validates_that_title_is_input(self):
+        form = FoodReviewForm(data={'content': 'hihihihihi'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['title'], [REQUIRED_ERROR_MESSAGE])
+
+    def test_validates_that_content_is_input(self):
+        form = FoodReviewForm(data={'title': 'hihihihi'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['content'], [REQUIRED_ERROR_MESSAGE])
