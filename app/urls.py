e042486640a74c703244d1a8fd89dbed311fffcf
MrMeta <bungyo92@gmail.com>
2020-09-18 09:48:00 +0900

    Add create_food && Use it in food_list.html

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.food_list, name='food_list'),
+    path('/craete', views.create_food, name='create_food')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Food
 from .forms import FoodForm
 
@@ -6,3 +6,11 @@ from .forms import FoodForm
 def food_list(request):
     foods = Food.objects.all()
     return render(request, 'app/food_list.html', {'foods': foods, 'form': FoodForm()})
+
+
+def create_food(request):
+    form = FoodForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('food_list')
+    return redirect('food_list')
