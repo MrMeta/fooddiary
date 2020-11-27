bc226493c3645c13a6f03f4c0ff9f13ac7a7a285
Jong In Park <bungyo92@gmail.com>
2020-11-28 08:36:14 +0900

    FD-6 Remove django views, forms, static files

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,1 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.food_list, name='food_list'),
-    path('add', views.create_food, name='create_food'),
-    path('<int:food_id>', views.food_detail, name='food_detail'),
-    path('<int:food_id>/reviews/add', views.create_review, name='create_review'),
-    path('store', views.store_list, name='store_list'),
-    path('store/add', views.create_store, name='create_store'),
-]
