c0024d2189d6bdc483c86153fe9e2f0075c7d25b
Jong In Park <bungyo92@gmail.com>
2020-11-28 08:50:30 +0900

    Add urls

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+import views
+
+router = DefaultRouter()
+router.register(r'stores', views.StoreViewSet)
+router.register(r'foods', views.FoodViewSet)
+router.register(r'food-reviews', views.FoodReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
