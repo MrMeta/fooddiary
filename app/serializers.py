c8155722420becfe463cacd4c5d04b3826dc1958
Jong In Park <bungyo92@gmail.com>
2020-11-28 08:29:06 +0900

    FD-6 Add serializers

---
+++ app/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from models import Store, Food, FoodReview
+
+
+class StoreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+
+class FoodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Food
+        fields = '__all__'
+
+
+class FoodReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FoodReview
+        fields = '__all__'
