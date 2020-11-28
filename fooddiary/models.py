aae7bd553fadd90a8f809638dde4bf59bf8175ce
Jong In Park <bungyo92@gmail.com>
2020-11-28 16:58:03 +0900

    FD-6 Fix created_date to be non-nullable

--- fooddiary/models.py
+++ fooddiary/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     deleted_date = models.DateTimeField(null=True)
 
     class Meta:
